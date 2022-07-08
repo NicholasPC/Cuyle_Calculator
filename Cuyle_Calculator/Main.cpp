0705b4178b3293eb31f5e9e26ac3a43b0f99ce9e
NicholasPC <NPCuyle@student.fullsail.edu>
2022-07-08 15:42:00 -0400

    more buttonssss!

--- Cuyle_Calculator/Main.cpp
+++ Cuyle_Calculator/Main.cpp
@@ -3,8 +3,20 @@
 wxBEGIN_EVENT_TABLE(Main, wxFrame)
 wxEND_EVENT_TABLE()
 
-Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(800, 600)) {
-	
+Main::Main() : wxFrame(nullptr, wxID_ANY, "Cuyle Calculator WIP", wxPoint(200, 200), wxSize(800, 600)) {
+	screen = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 10), wxSize(650, 30));
+	clearScreen = new wxButton(this, wxID_ANY, "C", wxPoint(690, 10), wxSize(70, 30));
+	zero = new wxButton(this, wxID_ANY, "0", wxPoint(20, 80), wxSize(400, 40));
+
+	wxGridSizer* numberPad = new wxGridSizer(numPX, numPY, 30, 100);
+
+	pad = new int[numPX * numPY];
+
+	for (int i = 0; i < numPX; i++) {
+		for (int j = 0; j < numPY; j++) {
+
+		}
+	}
 }
 
 Main::~Main() {
--- Cuyle_Calculator/Main.h
+++ Cuyle_Calculator/Main.h
@@ -8,6 +8,17 @@ public:
 	~Main();
 	wxDECLARE_EVENT_TABLE();
 
+public:
+	wxTextCtrl* screen = nullptr;
+	wxButton* zero = nullptr;
+	wxButton* clearScreen = nullptr;
+	int* numPad = nullptr;
+
+	int numPX = 3;
+	int numPY = 4;
+	int* pad = nullptr;
+
+
 private:
 	wxTextCtrl* calculatorBox = nullptr;
 };
