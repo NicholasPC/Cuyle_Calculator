e9c2c0e8cd741756135c7665438fdeab3873295c
NicholasPC <NPCuyle@student.fullsail.edu>
2022-07-08 19:27:45 -0400

    UI is FIIIIXXXXXEEED!!!!!!!!

--- Cuyle_Calculator/Main.cpp
+++ Cuyle_Calculator/Main.cpp
@@ -3,46 +3,46 @@
 wxBEGIN_EVENT_TABLE(Main, wxFrame)
 wxEND_EVENT_TABLE()
 
-Main::Main() : wxFrame(nullptr, wxID_ANY, "Cuyle Calculator WIP", wxPoint(200, 200), wxSize(670, 670)) {
+Main::Main() : wxFrame(nullptr, wxID_ANY, "Cuyle Calculator WIP", wxPoint(200, 200), wxSize(296, 390)) {
 
 	function = new wxButton * [17];
 
-	screen = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 10), wxSize(540, 70), wxTE_READONLY);
-	clearScreen = new wxButton(this, wxID_ANY, "C", wxPoint(570, 10), wxSize(70, 70));
+	screen = new wxTextCtrl(this, wxID_ANY, "", wxPoint(0, 0), wxSize(210, 70), wxTE_READONLY);
+	clearScreen = new wxButton(this, wxID_ANY, "C", wxPoint(210, 0), wxSize(70, 70));
 	function[0] = clearScreen;
 
-	zero = new wxButton(this, 11000, "0", wxPoint(60, 200), wxSize(40, 40));
+	zero = new wxButton(this, 11000, "0", wxPoint(70, 280), wxSize(70, 70));
 	function[1] = zero;
-	one = new wxButton(this, 11001, "1", wxPoint(20, 160), wxSize(40, 40));
+	one = new wxButton(this, 11001, "1", wxPoint(0, 210), wxSize(70, 70));
 	function[2] = one;
-	two = new wxButton(this, 11002, "2", wxPoint(60, 160), wxSize(40, 40));
+	two = new wxButton(this, 11002, "2", wxPoint(70, 210), wxSize(70, 70));
 	function[3] = two;
-	three = new wxButton(this, 11003, "3", wxPoint(100, 160), wxSize(40, 40));
+	three = new wxButton(this, 11003, "3", wxPoint(140, 210), wxSize(70, 70));
 	function[4] = three;
-	four = new wxButton(this, 11004, "4", wxPoint(20, 120), wxSize(40, 40));
+	four = new wxButton(this, 11004, "4", wxPoint(0, 140), wxSize(70, 70));
 	function[5] = four;
-	five = new wxButton(this, 11005, "5", wxPoint(60, 120), wxSize(40, 40));
+	five = new wxButton(this, 11005, "5", wxPoint(70, 140), wxSize(70, 70));
 	function[6] = five;
-	six = new wxButton(this, 11006, "6", wxPoint(100, 120), wxSize(40, 40));
+	six = new wxButton(this, 11006, "6", wxPoint(140, 140), wxSize(70, 70));
 	function[7] = six;
-	seven = new wxButton(this, 11007, "7", wxPoint(20, 80), wxSize(40, 40));
+	seven = new wxButton(this, 11007, "7", wxPoint(0, 70), wxSize(70, 70));
 	function[8] = seven;
-	eight = new wxButton(this, 11008, "8", wxPoint(60, 80), wxSize(40, 40));
+	eight = new wxButton(this, 11008, "8", wxPoint(70, 70), wxSize(70, 70));
 	function[9] = eight;
-	nine = new wxButton(this, 11009, "9", wxPoint(100, 80), wxSize(40, 40));
+	nine = new wxButton(this, 11009, "9", wxPoint(140, 70), wxSize(70, 70));
 	function[10] = nine;
 
-	mod = new wxButton(this, 11010, "%", wxPoint(10, 550), wxSize(70, 70));
+	mod = new wxButton(this, 11010, "%", wxPoint(0, 280), wxSize(70, 70));
 	function[11] = mod;
-	equals = new wxButton(this, 11011, "=", wxPoint(100, 200), wxSize(40, 40));
+	equals = new wxButton(this, 11011, "=", wxPoint(140, 280), wxSize(70, 70));
 	function[12] = equals;
-	plus = new wxButton(this, 11012, "+", wxPoint(570, 340), wxSize(70, 70));
+	plus = new wxButton(this, 11012, "+", wxPoint(210, 70), wxSize(70, 70));
 	function[13] = plus;
-	minus = new wxButton(this, 11013, "-", wxPoint(570, 410), wxSize(70, 70));
+	minus = new wxButton(this, 11013, "-", wxPoint(210, 140), wxSize(70, 70));
 	function[14] = minus;
-	multiply = new wxButton(this, 11014, "*", wxPoint(570, 480), wxSize(70, 70));
+	multiply = new wxButton(this, 11014, "*", wxPoint(210, 210), wxSize(70, 70));
 	function[15] = multiply;
-	divide = new wxButton(this, 11015, "/", wxPoint(570, 550), wxSize(70, 70));
+	divide = new wxButton(this, 11015, "/", wxPoint(210, 280), wxSize(70, 70));
 	function[16] = divide;
 
 	for (int i = 0; i < 17; i++) {
